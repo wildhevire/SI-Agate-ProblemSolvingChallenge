9c9a4e30efb53ddfc0a6459cf56aa14d4e000600
Wildhevire <wildhevire404@gmail.com>
2021-10-13 00:23:11 +0700

    problem 5 done

---
+++ Assets/Scripts/MoveToMousePosition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToMousePosition : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+    private Camera mainCamera;
+    public float speed;
+
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            var mouseWorld = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            var step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, mouseWorld, step);
+        }
+    }
+}
