d5e019c377ec17f59d5d18c18482e703197c6f67
Wildhevire <wildhevire404@gmail.com>
2021-10-13 00:49:56 +0700

    problem 7 done

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text scoreText;
+    private int currentScore = 0;
+    public static Action OnHitSquare = delegate{};
+
+    void Start()
+    {
+        SetScoreText(currentScore);
+    }
+
+    void OnEnable()
+    {
+        OnHitSquare += IncreaseScore;
+    }   
+
+    void OnDisable()
+    {
+        OnHitSquare -= IncreaseScore;
+    }
+
+    void SetScoreText(int score)
+    {
+        scoreText.text = "Score : " + score;
+    }
+
+
+    void IncreaseScore()
+    {
+        currentScore++;
+        SetScoreText(currentScore);
+    } 
+}
