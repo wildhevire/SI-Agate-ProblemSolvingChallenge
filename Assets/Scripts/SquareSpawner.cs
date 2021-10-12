0b51a054c682818f381f570a5f1709b359c24f7d
Wildhevire <wildhevire404@gmail.com>
2021-10-13 00:31:35 +0700

    problem 6 done

---
+++ Assets/Scripts/SquareSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareSpawner : MonoBehaviour
+{
+    public GameObject squarePrefab;
+    public int maxSquareCount;
+    public float maxX;
+    public float maxY;
+
+    void Start()
+    {
+        var totalSquare = Random.Range(0,maxSquareCount);
+        for (int i = 0; i < totalSquare; i++)
+        {
+            var xPosition = Random.Range(-maxX, maxX);
+            var yPosition = Random.Range(-maxY, maxY);
+            var pos = new Vector2(xPosition, yPosition);
+            Instantiate(squarePrefab, pos, Quaternion.identity);
+        }
+    }
+}
