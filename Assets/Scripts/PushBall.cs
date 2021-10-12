9e4850a2042837d9275c63470022a4f7798b1f99
Wildhevire <wildhevire404@gmail.com>
2021-10-12 23:52:22 +0700

    problem 2 done

---
+++ Assets/Scripts/PushBall.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PushBall : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+    public float speed;
+
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        Push();
+    }
+
+    private void Push()
+    {
+        var xForce = Random.Range(-1f, 1f);
+        var yForce = Random.Range(-1f, 1f);
+        var force = new Vector2(xForce, yForce);
+        rigidBody2D.velocity = force.normalized * speed;
+    }
+}
