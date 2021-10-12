887c5c5b97795dc794db3473ee5d5030e953a0e7
Wildhevire <wildhevire404@gmail.com>
2021-10-13 00:12:33 +0700

    problem 4 done

---
+++ Assets/Scripts/MoveWithArrowKey.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithArrowKey : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+    
+    private Vector2 direction;
+    public float speed;
+
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        direction = new Vector2(horizontal, vertical);
+    }
+
+    void FixedUpdate()
+    {
+        rigidBody2D.velocity = direction.normalized * speed;
+    }
+}
